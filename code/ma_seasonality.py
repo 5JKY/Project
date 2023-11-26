59ca6a4ab0bb8c9c582dc06918989d6945975ca1
ying <yjiang23@mun.ca>
2023-11-26 15:05:01 -0330

    add quarterly and yearly moving average seperately for comparision

--- code/ma_seasonality.py
+++ code/ma_seasonality.py
@@ -11,36 +11,29 @@ mpl.rcParams.update({
     'figure.figsize': [15, 8]
 })
 
+# resample the data to monthly frequency and computes a 12-month centered moving average
+selling_rates.resample('BMS').mean().rolling(window=12, center=True).mean().plot(style='--', label='12 month moving average')
+selling_rates.resample('BMS').mean().plot(alpha=0.3, style='-', label = 'Monthly Mean Exchange Rate')
 
-selling_rates.resample('BMS').mean().rolling(window=12, center=True).mean().plot(style='--', label=f'12 month moving average')
-selling_rates.resample('BMS').mean().plot(alpha=0.3, style='-', label = 'Exchange Rates Original Data')
-
-plt.title('Monthly Mean with Moving Average Overlay')
-plt.xlabel('Year')
+plt.title('Monthly Mean Exchange Rate with Yearly Moving Average Overlay')
+plt.xlabel('Date')
 plt.ylabel('Exchange Rate')
 plt.legend()
 plt.tight_layout()
-plt.savefig('graphs/monthly_MA.pdf')
+plt.savefig('graphs/yearly_MA.pdf')
 plt.show()
 
 
-# Perform seasonal decomposition, choose additive model
-result = seasonal_decompose(selling_rates.resample('BMS').mean(), model='additive', period=12)
+# operate MA on the original data with a window size of three months of business days(quarterly)
+# assuming an average of 21 business days per month, each quater has three months
+window = 21*3
+selling_rates.rolling(window, center=True).mean().plot(style='--', label=f'{window} bussine days moving average')
+selling_rates.plot(alpha=0.3, style='-', label = 'Daily Exchange Rate-orignal data')
 
-# subplot trend(moving average above), seasonal, residual component
-fig, axs = plt.subplots(3, 1, sharex=True)
-
-# Trend component
-axs[0].plot(result.trend, label='Trend Component')
-axs[0].legend(loc='lower left') 
-
-# Seasonal component
-axs[1].plot(result.seasonal, label='Seasonal Component')
-axs[1].legend(loc='lower left')
-
-# Residual component
-axs[2].plot(result.resid, label='Residual Component')
-axs[2].legend(loc='lower left')
+plt.title('Daily Exchange Rate with Quarterly Moving Average Overlay')
+plt.xlabel('Date')
+plt.ylabel('Exchange Rate')
+plt.legend()
 plt.tight_layout()
-plt.savefig('graphs/seasonal_decompose.pdf')
+plt.savefig('graphs/quarterly_MA.pdf')
 plt.show()
